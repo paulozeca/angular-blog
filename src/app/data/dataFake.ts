8af710bd5ee45465678ab856218b78390d40fa0d
paulozeca <paulozeca@gmail.com>
2024-02-24 14:10:35 -0300

    content created1

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -10,5 +10,19 @@ export const dataFake = [
      "title":"Saiu nova série",
      "description":"Olá id2!",
      "photoCover":"https://t.ctcdn.com.br/bBqQ0j6FNPyDxr05w8QkhuwpHI8=/768x432/smart/i798787.jpeg"
+   },
+   {
+    "id":"3",
+     "title":"Novidades no mundo Marvel!",
+     "description":"Novo filme anunciado para próximo mês!",
+     "photoCover":"https://images.universohq.com/2013/04/ironman3_poster.jpg"
+   },
+   {
+    "id":"4",
+     "title":"Novidades no mundo Marvel!",
+     "description":"Novo filme anunciado para próximo mês!",
+     "photoCover":"https://images.universohq.com/2013/05/homemdeferro3_resenha1.jpg"
    }
+
+
 ]
